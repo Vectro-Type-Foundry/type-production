5b0591a9245c1443dd249fbcc0acd06da6713929
Travis Kochel <travis@scribbletone.com>
2021-10-19 17:04:45 -0700

    trial font convert

--- trialfonts/trialfonts.sh
+++ trialfonts/trialfonts.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Usage:
-#  sh trialfonts.sh [path to source directory] [output path]
+#  sh trialfonts.sh [font name] [path to source directory] [output path]
 
 
 echo "Start Generate Trial Fonts"
@@ -13,12 +13,18 @@ FONT_TRIAL_NAME=$1
 SOURCE_DIRECTORY=$2
 OUTPUT_SUBDIRECTORY_NAME='trial-exports'
 
+echo $FONT_TRIAL_NAME
+echo $SOURCE_DIRECTORY
+
+
 if [ -z "$3" ]
 then
 	OUTPUT_DIRECTORY=$SOURCE_DIRECTORY/$OUTPUT_SUBDIRECTORY_NAME
 else
 	OUTPUT_DIRECTORY=$3	
 fi
+
+echo $OUTPUT_DIRECTORY
 
 FAMILY_NAME_SUFFIX='Trial'
 
@@ -36,41 +42,6 @@ function subsetFonts {
 		fi
 	done
 }
-
-# function renameFonts {
-# 	FORMAT=$1
-# 	for MODFILE in $OUTPUT_DIRECTORY/*.$FORMAT; do
-# 		if test -f "$MODFILE"; then
-# 			FONT=${MODFILE##*/}
-# 			TYPE=${FONT##*.}
-# 			NAME=${FONT%.*}
-# 			STYLE=${NAME##*-}
-# 			FAMILY=${FONT%%-*}
-# 			#Take file name and add spaces where uppercase
-# 			sed 's/\([^[:blank:]]\)\([[:upper:]]\)/\1 \2/g' <<< "${FAMILY}" > "${MODFILE%.???}".txt
-# 			sed ':a;N;$!ba;s/\n/ /g' ${MODFILE%.???}.txt > ${MODFILE%.???}-noline.txt
-# 			#Turn spaced file name into variable
-# 			FILECONTENTS=`cat ${MODFILE%.???}.txt`
-# 			#Delete file that sed created
-# 			rm ${MODFILE%.???}.txt ${MODFILE%.???}-noline.txt
-# 			#Dump font name tables
-# 			ttx -t name $MODFILE
-# 			# #Use sed again to find and replace in name tables font name with font name + Trial
-# 			sed -e "s:$FILECONTENTS:$FILECONTENTS $FAMILY_NAME_SUFFIX:g" ${MODFILE%.???}.ttx > ${MODFILE%.???}-$FAMILY_NAME_SUFFIX.ttx
-# 			# #Use ttx to fuse new nametable
-# 			ttx -m $MODFILE ${MODFILE%.???}-$FAMILY_NAME_SUFFIX.ttx
-# 			#Remove original font file
-# 			rm ${MODFILE%.???}.$FORMAT
-# 			#Remove name table files
-# 			rm -f ${MODFILE%.???}.ttx ${MODFILE%.???}-$FAMILY_NAME_SUFFIX.ttx
-# 			echo ${FONT} Complete
-# 		fi
-# 	done
-# }
-# cd docs
-# zip -r ${timestamp}_VeryCool_TrialFonts.zip VeryCool-TrialFonts
-
-
 function renameFonts {
 	FORMAT=$1
 	for FILE in $OUTPUT_DIRECTORY/*.$FORMAT; do
