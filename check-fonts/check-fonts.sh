0a3c4df221fb0672a50440d337658db1c1899349
Travis Kochel <980360+traviskochel@users.noreply.github.com>
2022-05-03 10:30:42 -0700

    font checker

---
+++ check-fonts/check-fonts.sh
@@ -0,0 +1,24 @@
+echo "Start Checking Fonts in ./src"
+
+# sourcePath=$1
+# outputPath=$2
+sourcePath="./src/"
+outputPath="./output/"
+
+function runFontBakeryCheck {
+  
+  # echo $2
+  for fileFullPath in $sourcePath*$1; do 
+    if [ -e $fileFullPath ]
+    then
+      filename=`basename $fileFullPath`
+      echo $filename
+      fontbakery check-typenetwork $fileFullPath  --json "${outputPath}fontbakery-report-${filename}.json"
+    fi
+  done
+}
+
+runFontBakeryCheck .otf
+runFontBakeryCheck .ttf
+
+echo "Finished!"
